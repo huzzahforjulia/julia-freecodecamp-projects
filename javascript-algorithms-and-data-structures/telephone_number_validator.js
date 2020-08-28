e5ab9c20cea5f4b225ab5572af10a4d47d750eb9
Julia <49330502+huzzahforjulia@users.noreply.github.com>
2020-08-27 22:38:18 -0400

    Update telephone_number_validator.js

--- javascript-algorithms-and-data-structures/telephone_number_validator.js
+++ javascript-algorithms-and-data-structures/telephone_number_validator.js
@@ -1,4 +1,5 @@
 // Note: You could also do this in a one-line regex.
+// I find this way is more readable and clearly shows all the different cases (despite being quite repetitive)
 function telephoneCheck(str) {
   if(/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(str)) { // 555-555-5555
     return true;
